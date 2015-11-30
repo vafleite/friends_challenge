e63befaaced500d46d2d30c933292bc73e2409f3
Victor <victor.furtadoleite@gmail.com>
2015-11-30 09:03:00 -0200

    Changed from function to class based view.
    Modified API url

--- friends/urls.py
+++ friends/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from friends import views
 
 urlpatterns = [
-    url(r'^start$', views.start),
+    url(r'^me$', views.Me.as_view()),
 ]
 
--- friends/views.py
+++ friends/views.py
@@ -1,27 +1,25 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from friends.models import Friend
 from friends.serializers import FriendSerializer
 
 
-@api_view(['GET', 'POST'])
-@csrf_exempt
-def start(request):
-    from random import randint
+class Me(APIView):
 
-    if request.method == 'GET':
-        x = randint(0, 100)
-        y = randint(0, 100)
-        me = Friend(x=x, y=y, me=True)
-        me.save()
-        me_serialized = FriendSerializer(me)
-        return Response(me_serialized.data)
+    def get(self, request):
+        if request.method == 'GET':
+            me = Friend(me=True)
+            me.save()
+            me.generate_friends()
+            me_serialized = FriendSerializer(me)
+            return Response(me_serialized.data)
 
 
 
